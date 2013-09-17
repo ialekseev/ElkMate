6a18dd5e916196baa0d839a5b8fd70adfb21d884
Igor Alekseev <alekseevi15@gmail.com>
2013-09-17 18:04:31 +0400

    Renaming output assemblies

--- src/main/ElkMate-Common/Properties/AssemblyInfo.cs
+++ src/main/ElkMate-Common/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SmartElk-ElkMate-Common")]
+[assembly: AssemblyTitle("SmartElk.ElkMate.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SmartElk")]
