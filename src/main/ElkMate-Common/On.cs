c2897cd553782598c53ed27d2e86ffd3d5fc889d
Igor Alekseev <alekseevi15@gmail.com>
2013-09-24 16:27:08 +0400

    'On' class to apply operations and perform commands + specs

---
+++ src/main/ElkMate-Common/On.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartElk.ElkMate.Common
+{
+    public class On<T>
+    {
+        public Func<IEnumerable<T>> GetItems { get; set; }
+        public List<Action<T>> ToApply { get; set; }
+        public List<Action> ToPerform { get; set; }
+                
+        private On()
+        {
+            ToApply = new List<Action<T>>();
+            ToPerform = new List<Action>();            
+        }
+
+        public static On<T> Items(Func<IEnumerable<T>> getItems)
+        {            
+            return new On<T>(){GetItems = getItems};
+        }
+
+        public On<T> Apply(Action<T> apply)
+        {
+            this.ToApply.Add(apply);
+            return this;
+        }
+
+        public On<T> Perform(Action perform)
+        {
+            this.ToPerform.Add(perform);
+            return this;
+        }
+
+        public IList<T> Execute()
+        {           
+            var result = GetItems().ToList();
+            
+            foreach (var item in result)
+            {                
+                foreach (var apply in ToApply)
+                {
+                    apply(item);
+                }                
+            }
+
+            foreach (var perform in ToPerform)
+            {
+                perform();
+            }
+                        
+            return result;
+        }        
+    }
+}
---
+++ src/specs/ElkMate-Common-Specs/OnSpecs.cs
@@ -0,0 +1,115 @@
+﻿// ReSharper disable InconsistentNaming
+using System.Collections.Generic;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SmartElk.ElkMate.Common.Specs
+{
+    public class OnSpecs
+    {        
+        public class BaseOnSpec
+        {
+            public class Item
+            {
+                public string A { get; set; }
+                public string B { get; set; }
+            }
+
+            public interface IClass
+            {
+                void DoSomething();
+            }
+        }
+        
+        
+        [TestFixture]
+        public class when_trying_to_apply_and_perform_operations: BaseOnSpec
+        {                                    
+            
+            [Test]
+            public void should_return_valid_result()
+            {
+                var someObject = A.Fake<IClass>();
+
+                var result =
+                    On<Item>.Items(() => new List<Item>() {new Item() {A = "1", B = "1"}, new Item() {A = "2", B = "2"}})
+                            .Apply(t =>
+                                {
+                                    t.A = "3";
+                                    t.B = "4";
+                                }).
+                             Perform(someObject.DoSomething).
+                             Execute();
+                   
+
+                result.Count.Should().Be(2);
+                result[0].A.Should().Be("3");
+                result[0].B.Should().Be("4");
+                result[1].A.Should().Be("3");
+                result[1].B.Should().Be("4");
+
+                A.CallTo(() => someObject.DoSomething()).MustHaveHappened();
+            }
+        }
+       
+        [TestFixture]
+        public class when_trying_to_perform_two_times: BaseOnSpec
+        {            
+            [Test]
+            public void should_perform_two_times()
+            {
+                var someObject = A.Fake<IClass>();
+
+                var result =
+                    On<Item>.Items(() => new List<Item>() {new Item() {A = "1", B = "1"}, new Item() {A = "2", B = "2"}})
+                            .
+                             Perform(someObject.DoSomething).
+                             Perform(someObject.DoSomething).
+                             Execute();
+                   
+
+                result.Count.Should().Be(2);
+                result[0].A.Should().Be("1");
+                result[0].B.Should().Be("1");
+                result[1].A.Should().Be("2");
+                result[1].B.Should().Be("2");   
+
+                A.CallTo(() => someObject.DoSomething()).MustHaveHappened(Repeated.Exactly.Twice);
+            }
+        }
+
+        [TestFixture]
+        public class when_trying_to_apply_multiple_times: BaseOnSpec
+        {            
+            [Test]
+            public void should_return_valid_result()
+            {
+                var someObject = A.Fake<IClass>();
+
+                var result =
+                    On<Item>.Items(() => new List<Item>() {new Item() {A = "1", B = "1"}, new Item() {A = "2", B = "2"}})
+                            .Apply(t =>
+                                {
+                                    t.A = "3";
+                                })
+                            .Apply(t =>
+                                {
+                                    t.B = "4";
+                                }).
+                             Perform(someObject.DoSomething).
+                             Execute();                   
+
+                result.Count.Should().Be(2);
+                result[0].A.Should().Be("3");
+                result[0].B.Should().Be("4");
+                result[1].A.Should().Be("3");
+                result[1].B.Should().Be("4");
+
+                A.CallTo(() => someObject.DoSomething()).MustHaveHappened();
+            }
+        }
+    }
+}
+
+// ReSharper restore InconsistentNaming
