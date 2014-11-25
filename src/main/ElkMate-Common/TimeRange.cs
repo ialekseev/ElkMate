124228374e67ab0c10c4fa285ca227b3cce26f12
Igor Alekseev <alekseevi15@gmail.com>
2014-11-25 15:02:18 +0400

    Adding TimeRange class

---
+++ src/main/ElkMate-Common/TimeRange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SmartElk.ElkMate.Common
+{
+    public struct TimeRange
+    {
+        public TimeSpan From { get; private set; }
+        public TimeSpan To { get; private set; }
+
+        public TimeRange(TimeSpan @from, TimeSpan to) : this()
+        {
+            From = @from;
+            To = to;
+        }
+        
+        public bool IsBetween(TimeSpan timeSpan)
+        {
+            var now = timeSpan;
+            if (From < To)
+                return From <= now && now <= To;
+            return !(To < now && now < From);
+        }
+
+        public bool IsBetween(DateTime datetime)
+        {
+            return IsBetween(datetime.TimeOfDay);
+        }
+    }
+}
